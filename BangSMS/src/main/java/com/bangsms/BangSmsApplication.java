65a8f970e16af30bf456fa70d04c5081f1f62581
Himanshu Matta <himanshu.matta@soltius.co.nz>
2017-03-10 14:23:35 +1300

    Code base pushed

---
+++ BangSMS/src/main/java/com/bangsms/BangSmsApplication.java
@@ -0,0 +1,12 @@
+package com.bangsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BangSmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BangSmsApplication.class, args);
+	}
+}
